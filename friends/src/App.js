d6920dd86d9ea472a1375499b160b49606719c72
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-27 13:07:39 +0000

    MVP: Created FriendsList component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import FriendsList from './components/FriendsList';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then(response => this.setState({ friends: response.data }))
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { friends } = this.state;
     return (
-      <div className="App">
-        <FriendsList />
+      <div className="friends-wrapper">
+        <FriendsList friends={friends} />
       </div>
     );
   }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const FriendsList = (props) => {
   return(
-    <h1>List of Friends</h1>
+    <div className="friends-list-container">
+      <h1>List of Friends</h1>
+      {props.friends.map(friend => {
+      return (
+        <div className="friend">
+          <p>Name: {friend.name}</p>
+          <p>Age: {friend.age}</p>
+          <p>Email: <a href="#">{friend.email}</a></p>
+        </div>
+      )
+    })}
+    </div>
+    
   )
 }
 
