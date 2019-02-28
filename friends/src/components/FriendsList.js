417dd9e3eeffc792dad5ccb168a5fb48a72ae4c3
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-28 07:43:50 +0000

    MVP: Refactored FriendsList

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import Friend from './Friend';
+import FriendEditable from './FriendEditable';
 
 const FriendsList = (props) => {
   return(
-    <div className="friends-list-container">
+    <div className="friends-list-container" key={props.id}>
       <h2>List of Friends</h2>
       {props.friends.map(friend => {
-      return (
-        <div className="friend-container" key={friend.id}>
-          <div className="details">
-            <p>Name: {friend.name}</p>
-            <p>Age: {friend.age}</p>
-            <p>Email: <a href="#">{friend.email}</a></p>
-          </div>
-          <div className="requests">
-            <button className="update">Update</button>
-            <button className="delete" onClick={(e) => {props.deleteFriend(e,friend.id)}}>Delete</button>
-          </div>
-        </div>
-      )
-    })}
+        if (friend.isEditable) {
+          return (
+            <FriendEditable
+              name={friend.name}
+              age={friend.age}
+              id={friend.id}
+              email={friend.email}
+              updateFriend={props.updateFriend}
+              handleUpdateChange={props.handleUpdateChange}
+              makeUneditable={props.makeUneditable}
+            />);
+        }
+        return (
+          <Friend
+            name={friend.name}
+            age={friend.age}
+            id={friend.id}
+            key={friend.id}
+            email={friend.email}
+            deleteFriend={props.deleteFriend}
+            makeEditable={props.makeEditable}
+          />
+        );
+      })}
     </div>
     
   )
